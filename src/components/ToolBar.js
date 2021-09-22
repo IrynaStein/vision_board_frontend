532bb4a1752d889d8daa8a148ef0d77b80c8b8bf
Iryna Stein <isophiestein@gmail.com>
2021-09-21 21:40:50 -0600

    added files to gitignore

--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { userLogout } from "../store/utilitySlice";
+import apiKey from "../api";
 export default function ToolBar() {
   const user = useSelector((state) => state.utilities.user);
   const dispatch = useDispatch();
@@ -15,7 +16,7 @@ export default function ToolBar() {
     //   "method": "GET",
     //   "headers": {
     //     "x-rapidapi-host": "quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com",
-    //     "x-rapidapi-key": "444860251cmsh0fb14120439fb6fp1fe83djsn6801ad7d9839"
+    //     "x-rapidapi-key": apiKey
     //   }
     // })
     // .then(resp => resp.json())
@@ -30,7 +31,7 @@ export default function ToolBar() {
       method: "GET",
       headers: {
         "x-rapidapi-host": "quotes15.p.rapidapi.com",
-        "x-rapidapi-key": "444860251cmsh0fb14120439fb6fp1fe83djsn6801ad7d9839",
+        "x-rapidapi-key": apiKey
       },
     })
       .then((resp) => resp.json())
@@ -49,14 +50,14 @@ export default function ToolBar() {
         <Link to="/water" className="element-link">
           <img src="https://live.staticflickr.com/65535/51499110765_a3f537a2c4_o.png" alt="water symbol"/>
         </Link>
-        <Link to="/fire" className="element-link">
-          <img src="https://live.staticflickr.com/65535/51498183701_90f7ba7f6e_o.png" alt="fire symbol"/>
+        <Link to="/earth" className="element-link">
+          <img src="https://live.staticflickr.com/65535/51498899924_746037b32f_o.png" alt="earth symbol"/>
         </Link>
         <Link to="/air" className="element-link">
           <img src="https://live.staticflickr.com/65535/51498405518_35f832371a_o.png" alt="air symbol"/>
         </Link>
-        <Link to="/earth" className="element-link">
-          <img src="https://live.staticflickr.com/65535/51498899924_746037b32f_o.png" alt="earth symbol"/>
+        <Link to="/fire" className="element-link">
+          <img src="https://live.staticflickr.com/65535/51498183701_90f7ba7f6e_o.png" alt="fire symbol"/>
         </Link>
       </div>
       <input placeholder="affirmation..."></input>
